072d3c4352d5fb8576cfce64ef52ba965736afb4
David Swift <davidswift@Davids-MacBook-Pro.local>
2019-12-11 23:40:00 +0100

    Fix stop seed data generation and dump to csv

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -2,71 +2,89 @@ class Stop < ApplicationRecord
   belongs_to :cruise
   belongs_to :port
 
-  def self.seed_data
-    Cruise.all.each_with_index do |cruise, i|
-      sleep 2
+  def self.generate_seed_data
+    require 'csv'
 
-      response = Typhoeus.get(
-        "https://www.msccruisesusa.com/en-us/Plan-Book/Itinerary-detail-b2c.aspx?cruiseid=#{cruise.code}",
-        headers: { "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36" }
-      )
-      
-      parsed_body = Nokogiri::HTML(response.body)
-      
-      departure_datetime = nil
-      arrival_datetime = nil
-      departure_port = nil
-      arrival_port = nil
-      
-      parsed_body.xpath('(//tr)')[1..-1].each do |row|
-        cells = row.children.map(&:text).map(&:strip).reject { |x| x == '' }
-        
-        if departure_datetime.nil?
-          departure_datetime = cells[1]
-          departure_port = cells[2]
-          next
-        elsif arrival_datetime.nil?
-          if cells[3] == '-'
-            next
-          else
-            arrival_datetime = cells[1]
-            arrival_port = cells[2]
-          end
-        end
+    CSV.open("stops_data.csv", "wb") do |csv|
+      csv << ["cruise_id", "port_id", "d_from", "d_to"]
 
-        from_time, to_time = cells[3..4].map do |time|
-          time.present? && time != '-' ? time : ''
-        end
+      Cruise.all.each_with_index do |cruise, i|
+        return unless i < 10
+        sleep 2
 
-        d_from = (cells[1] + ' ' + from_time).strip
-        d_to   = (cells[1] + ' ' + to_time).strip
-
-        port_and_country_name = cells[2].strip
-        port_name = port_and_country_name.split(',').first
-        port = Port.find_by("name ILIKE '#{port_name}%'")
-        port ||= Port.create(name: port_and_country_name)
-
-        Stop.create(
-          cruise_id: cruise.id,
-          port_id: port.id,
-          d_from: d_from,
-          d_to: d_to
+        response = Typhoeus.get(
+          "https://www.msccruisesusa.com/en-us/Plan-Book/Itinerary-detail-b2c.aspx?cruiseid=#{cruise.code}",
+          headers: { "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36" }
         )
+
+        parsed_body = Nokogiri::HTML(response.body)
 
         departure_datetime = nil
         arrival_datetime = nil
         departure_port = nil
         arrival_port = nil
+        from_time = nil
+        to_time = nil
 
-        if cells[4] != '-'
-          departure_datetime = cells[1]
-          departure_port = cells[2]
+        rows = parsed_body.xpath('(//tr)')[1..-1]
+        rows.each_with_index do |row, i|
+          cells = row.children.map(&:text).map(&:strip).reject { |x| x == '' }
+
+          current_date = cells[1]
+          current_arrival_time = cells[3] != '-' ? cells[3] : nil
+          current_departure_time = cells[4] != '-' ? cells[4] : nil
+
+          current_port_and_country_name = cells[2].strip
+          current_port_name = current_port_and_country_name.split(',').first
+          current_port = Port.find_by("name ILIKE '#{current_port_name}%'")
+          current_port ||= Port.create(name: current_port_and_country_name)
+
+          next if current_arrival_time.nil? && current_departure_time.nil?
+
+          if i.zero?
+            csv << [
+              cruise.id,
+              current_port.id,
+              current_date,
+              current_date + ' ' + current_departure_time
+            ]
+            next
+          end
+
+          if i == rows.length - 1
+            csv << [
+              cruise.id,
+              current_port.id,
+              current_date + ' ' + current_arrival_time,
+              current_date
+            ]
+            next
+          end
+
+          if current_arrival_time && current_departure_time
+            csv << [
+              cruise.id,
+              current_port.id,
+              current_date + ' ' + current_arrival_time,
+              current_date + ' ' + current_departure_time
+            ]
+          elsif current_arrival_time
+            @multi_day_arrival_date = current_date
+            @multi_day_arrival_time = current_arrival_time
+          else
+            csv << [
+              cruise.id,
+              current_port.id,
+              @multi_day_arrival_date + ' ' + @multi_day_arrival_time,
+              current_date + ' ' + current_departure_time
+            ]
+          end
         end
+      rescue => e
+        puts "Failed to import cruise #{cruise.code}"
+        puts "Error: #{e.message}"
+        next
       end
-    rescue => e
-      puts "Failed to import cruise #{cruise.code}"
-      puts "Error: #{e.message}"
-      next
     end
   end
 end
